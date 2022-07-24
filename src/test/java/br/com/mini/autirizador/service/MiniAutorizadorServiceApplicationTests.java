acc33ac4ce3cd3727c3ab2bd28d616e92e91979b
tiagopastor <tpastor@magnasistemas.com.br>
2022-07-24 15:56:53 -0300

    buid: Configurando o Dockerfile para gerar a docker image

--- src/test/java/br/com/mini/autirizador/service/MiniAutorizadorServiceApplicationTests.java
+++ src/test/java/br/com/mini/autirizador/service/MiniAutorizadorServiceApplicationTests.java
@@ -1,10 +1,12 @@
 package br.com.mini.autirizador.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@SpringBootConfiguration
 class MiniAutorizadorServiceApplicationTests {
 
 	@Test
